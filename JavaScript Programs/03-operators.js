7c0f0666e356c9a270aa4f9c37e2feb99328906f
Sumit Raskar <136845497+SumitRaskar01@users.noreply.github.com>
2024-05-14 23:43:33 +0530

    Day 2

---
+++ JavaScript Programs/03-operators.js
@@ -0,0 +1,94 @@
+/*
+    ->There are 4 types of Operators
+        ->Arithmetic Operator
+            -> "+" Example- let c = a + b;
+
+            -> "-" Example- let c = a - b;
+
+            -> "*" Example- let c = a * b;
+
+            -> "/" Example- let c = a / b;
+
+            -> "%" Modulus Operator. Example- let c = a % b;
+
+            -> "**" Exponential Operator. Example- let c = a ** b; i.e 5 to the power 2.
+
+            -> "++" Increment Operator. Example- let c = a++;
+                Post Increment a++;
+                Pre Increment ++a; 
+
+            -> "--" Decrement Operaotr. Example- let c = b--;
+                Post Decrement a--;
+                Pre Decrement --a;
+
+        ->Assignment Operator-
+            -> "=" Assignment Operator. Example- let age= 18;
+
+            -> "+=" Plus Equal to Operator. Example- a += 4;
+
+            -> "-=" Minus Equal to Operator. Example- a -= 4;
+
+            ->"*=" Multiply Equal to Operator. Example- a *= 4;
+
+            ->"/=" Modulus Equal to Operator. Example- a /= 4;
+
+            "**=" Exponential Equal to Operator. Example- a **= 4;
+
+        ->Comparision Operators 
+            -> "==" Equal to Operator. Example- a == b;
+
+            -> "!=" Not Equal to Operator. Example- a != b;
+
+            -> "===" Equal to & type Operator. Example- a === b;
+
+            -> "!==" Not Equal to & type Operator. Example- a !== b;
+
+            -> ">" Greater than operator. Example- a > b;
+
+            -> "<" Less than operator. Example- a < b;
+
+            -> "<=" Less than equal to operator. Example- a <= b;
+
+            -> ">=" Greater than equal to operator. Example- a >= b;
+
+        ->Logical Operator
+            -> "&&" Logical AND Operator. Example- cond1 && cond2;
+
+            -> "||" Logical OR Operator. Example- cond1 || cond2;
+
+            -> "!" Logical NOT Operator. Example- cond1 ! cond2;
+*/
+
+let a = 5;
+let b = 4;
+let c;
+
+console.log("Arithmetic Operators");
+console.log("a + b = " , a + b);
+console.log("a - b = " , a - b);
+console.log("a * b = " , a * b);
+console.log("a / b = " , a / b);
+console.log("a % b = " , a % b);
+console.log("a ** b = " , a ** b);
+console.log("Post Increment- a++ = " , a++);
+console.log("Pre Increment- ++a = " , ++a);
+console.log("Post Decrement- a-- = " , a--);
+console.log("Pre Decrement- --a = " , --a);
+
+console.log("Assignment Operators");
+console.log("a += 4 = ", a += 4);
+console.log("a -= 4 = ", a -= 4);
+console.log("a *= 4 = ", a *= 4)
+console.log("a /= 4 = ", a /= 4);
+console.log("a **= 4 = ", a ** 4);
+
+console.log("Comparision Operators");
+console.log("a == b", a == b);
+console.log("a != b", a != b);
+console.log("a === b", a === b);
+console.log("a!== b", a!== b);
+console.log("a > b", a > b);
+console.log("a < b", a < b);
+console.log("a <= b", a <= b);
+console.log("a >= b", a >= b);
+
