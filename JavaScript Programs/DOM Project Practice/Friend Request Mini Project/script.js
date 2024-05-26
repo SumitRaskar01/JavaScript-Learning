aeac3add9f04714602ca39277bad546c2b1604aa
Sumit Raskar <136845497+SumitRaskar01@users.noreply.github.com>
2024-05-26 21:39:32 +0530

    Delete JavaScript Programs/DOM Project Practice/Friend Request Mini Project directory

--- JavaScript Programs/DOM Project Practice/Friend Request Mini Project/script.js
+++
@@ -1,9 +0,0 @@
-let btn = document.querySelector(".btn");
-btn.addEventListener("click", () => {
-    if(btn.innerText === "Add Friend"){
-        btn.innerText = "Pending";
-    }
-    else{
-        btn.innerText = "Add Friend";
-    }
-})
